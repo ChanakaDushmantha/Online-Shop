00373fa46fd50138c47e83c84e2800102b5ea8b4
ChanakaDushmantha <2016sp314@gmail.com>
2020-04-29 18:55:35 +0530

    register complete

--- app/src/main/java/lk/chanaka/dushmantha/groceryonline/Register.java
+++ app/src/main/java/lk/chanaka/dushmantha/groceryonline/Register.java
@@ -1,23 +1,120 @@
 package lk.chanaka.dushmantha.groceryonline;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+
+    private EditText name, email, address, mobile, password, c_password;
+    private ProgressBar loading;
+    private CardView btn_register;
+    private static String URL_REGIST = "http://10.0.2.2:8000/api/register";//should use 10.0.2.2 for local host, laravel 10.0.2.2:8000
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        loading = findViewById(R.id.loading);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        address = findViewById(R.id.address);
+        mobile = findViewById(R.id.mobile);
+        password = findViewById(R.id.password);
+        c_password = findViewById(R.id.c_password);
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Regist();
+            }
+        });
+
     }
 
-    public void register(View view) {
+    private void Regist(){
+        loading.setVisibility(View.VISIBLE);
+        btn_register.setVisibility(View.GONE);
+
+        final String name = this.name.getText().toString().trim();
+        final String email = this.email.getText().toString().trim();
+        final String address = this.address.getText().toString().trim();
+        final String mobile = this.mobile.getText().toString().trim();
+        final String password = this.password.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success =  jsonObject.getString("success");
+
+                            if(success.equals("true")){
+                                Toast.makeText(Register.this, "Register Success!", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(Register.this, "Register Error 1 ! "+e.toString(), Toast.LENGTH_LONG).show();
+
+                            //loading.setVisibility(View.GONE);
+                            //btn_register.setVisibility(View.VISIBLE);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Register.this, "Register Error 2 ! "+error.toString(), Toast.LENGTH_LONG).show();
+                        System.out.println(error.toString());
+                        //loading.setVisibility(View.GONE);
+                        //btn_register.setVisibility(View.VISIBLE);
+
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", name);
+                params.put("email", email);
+                params.put("address", address);
+                params.put("contact_no", mobile);
+                params.put("password", password);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    /*public void register(View view) {
         Intent intent = new Intent(this, ProfilePicture.class);
         startActivity(intent);
-    }
+    }*/
 
     public void loginActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);

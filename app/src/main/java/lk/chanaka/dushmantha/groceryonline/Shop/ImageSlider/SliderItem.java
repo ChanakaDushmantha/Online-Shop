41560f3f945ad7eb5bb76ec597821e355a617c22
ChanakaDushmantha <2016sp314@gmail.com>
2020-07-16 11:09:15 +0530

    image slider

---
+++ app/src/main/java/lk/chanaka/dushmantha/groceryonline/Shop/ImageSlider/SliderItem.java
@@ -0,0 +1,23 @@
+package lk.chanaka.dushmantha.groceryonline.Shop.ImageSlider;
+
+public class SliderItem {
+
+    private String description;
+    private String imageUrl;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
--- app/src/main/java/lk/chanaka/dushmantha/groceryonline/Shop/ShopActivity.java
+++ app/src/main/java/lk/chanaka/dushmantha/groceryonline/Shop/ShopActivity.java
@@ -5,7 +5,9 @@ import androidx.cardview.widget.CardView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -23,6 +25,10 @@ import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.IndicatorView.draw.controller.DrawController;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -35,19 +41,17 @@ import lk.chanaka.dushmantha.groceryonline.Items.MainActivity;
 import lk.chanaka.dushmantha.groceryonline.MyApp;
 import lk.chanaka.dushmantha.groceryonline.R;
 import lk.chanaka.dushmantha.groceryonline.SessionManager;
+import lk.chanaka.dushmantha.groceryonline.Shop.ImageSlider.SliderAdapter;
+import lk.chanaka.dushmantha.groceryonline.Shop.ImageSlider.SliderItem;
 
 public class ShopActivity extends AppCompatActivity {
 
-    //private ProgressBar loading;
-    private CardView btn_next;
-    private ListView shoplist;
     private static String URL = "";
     private String host;
-    //private String token;
-    private int shopId = -1;
     ArrayList<Shop> shophouses;
     SessionManager sessionManager;
-    ListAdapter listAdapter;
+    SliderView sliderView;
+    private SliderAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,65 +59,61 @@ public class ShopActivity extends AppCompatActivity {
         setContentView(R.layout.activity_shops);
 
         sessionManager = new SessionManager(this);
-        //token = sessionManager.getToken();
 
         host = ((MyApp) this.getApplication()).getServiceURL();
-        URL = host+"/getAllShop";
 
-        //loading = findViewById(R.id.loading);
-        //btn_next = findViewById(R.id.btn_next);
-        //shoplist = findViewById(R.id.shoplist);
 
-        //loading.setVisibility(View.VISIBLE);
         shophouses = new ArrayList<>();
 
-        /*if(!(new NetworkConnection( this).isNetworkConnected())){
-            Toast.makeText(this, "Internet Connection Error", Toast.LENGTH_LONG).show();
-        }
-        else{*/
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try{
-                                JSONObject jsonObject = new JSONObject(response);
-                                String success = jsonObject.getString("success");
-                                JSONArray data = jsonObject.getJSONArray("data");
+        extractShop();
+        imageSlider();
+    }
 
-                                if(success.equals("true")){
-                                    //Toast.makeText(ShopActivity.this, "Shop Success!", Toast.LENGTH_SHORT).show();
-                                    //createList(data);
-                                    setShop(data);
-                                    //loading.setVisibility(View.GONE);
-                                    findViewById(R.id.shimmerShop).setVisibility(View.GONE);
+    private void extractShop(){
+        URL = host+"/getAllShop";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            JSONArray data = jsonObject.getJSONArray("data");
 
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                                Toast.makeText(ShopActivity.this, "Response type error "+e.toString(), Toast.LENGTH_LONG).show();
+                            if(success.equals("true")){
+                                //Toast.makeText(ShopActivity.this, "Shop Success!", Toast.LENGTH_SHORT).show();
+                                //createList(data);
+                                setShop(data);
+                                //loading.setVisibility(View.GONE);
+                                findViewById(R.id.shimmerShop).setVisibility(View.GONE);
+
                             }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(ShopActivity.this, "Response type error "+e.toString(), Toast.LENGTH_LONG).show();
                         }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            String errorMsg = "Error";
-                            if (error instanceof NoConnectionError) {
-                                errorMsg = getString(R.string.noConnectionError);
-                            } else if (error instanceof TimeoutError) {
-                                errorMsg = getString(R.string.timeoutError);
-                            } else if (error instanceof AuthFailureError) {
-                                errorMsg = getString(R.string.authFailureError);
-                            } else if (error instanceof ServerError) {
-                                errorMsg = getString(R.string.serverError);
-                            } else if (error instanceof NetworkError) {
-                                errorMsg = getString(R.string.networkError);
-                            } else if (error instanceof ParseError) {
-                                errorMsg = getString(R.string.parseError);
-                            }
-                            Toast.makeText(ShopActivity.this, errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        String errorMsg = "Error";
+                        if (error instanceof NoConnectionError) {
+                            errorMsg = getString(R.string.noConnectionError);
+                        } else if (error instanceof TimeoutError) {
+                            errorMsg = getString(R.string.timeoutError);
+                        } else if (error instanceof AuthFailureError) {
+                            errorMsg = getString(R.string.authFailureError);
+                        } else if (error instanceof ServerError) {
+                            errorMsg = getString(R.string.serverError);
+                        } else if (error instanceof NetworkError) {
+                            errorMsg = getString(R.string.networkError);
+                        } else if (error instanceof ParseError) {
+                            errorMsg = getString(R.string.parseError);
                         }
-                    })
+                        Toast.makeText(ShopActivity.this, errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                })
             /*{
                 @Override
                 public Map<String, String> getHeaders() throws AuthFailureError {
@@ -123,49 +123,106 @@ public class ShopActivity extends AppCompatActivity {
                     return params;
                 }
             }*/;
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
-            requestQueue.add(stringRequest);
-        //}
-
-       /* shoplist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                shopId = position+1;
-            }
-        });*/
-
-        /*btn_next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(shopId == -1) {
-                    Toast.makeText(ShopActivity.this, "Select which shop you favor to buy", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    //System.out.println(shopId);
-                    sessionManager.addShop(shopId);
-                    Intent intent = new Intent(ShopActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        });*/
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
-    /*private void createList(JSONArray array) throws JSONException {
-        ArrayList<String> arrayList = new ArrayList<>();
-        for(int i=0; i<array.length(); i++){
-           JSONObject jsonObject = array.getJSONObject(i);
-           String name = jsonObject.getString("name");
-           arrayList.add(name);
+    private void extractBanners(){
+        URL = host+"/getBannerImages";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            JSONArray data = jsonObject.getJSONArray("data");
+
+                            if(success.equals("true")){
+                                setBanners(data);
+                                //findViewById(R.id.shimmerShop).setVisibility(View.GONE);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(ShopActivity.this, "Response type error "+e.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        String errorMsg = "Error";
+                        if (error instanceof NoConnectionError) {
+                            errorMsg = getString(R.string.noConnectionError);
+                        } else if (error instanceof TimeoutError) {
+                            errorMsg = getString(R.string.timeoutError);
+                        } else if (error instanceof AuthFailureError) {
+                            errorMsg = getString(R.string.authFailureError);
+                        } else if (error instanceof ServerError) {
+                            errorMsg = getString(R.string.serverError);
+                        } else if (error instanceof NetworkError) {
+                            errorMsg = getString(R.string.networkError);
+                        } else if (error instanceof ParseError) {
+                            errorMsg = getString(R.string.parseError);
+                        }
+                        Toast.makeText(ShopActivity.this, errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                })
+            /*{
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    //params.put("Authorization", "Bearer "+token);
+                    //System.out.println(token);
+                    return params;
+                }
+            }*/;
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+    private void setBanners(JSONArray data) {
+        List<SliderItem> sliderItemList = new ArrayList<>();
+        for (int i = 0; i < data.length(); i++) {
+            try {
+                JSONObject detailsObject = data.getJSONObject(i);
+
+                SliderItem sliderItem = new SliderItem();
+                sliderItem.setImageUrl(detailsObject.getString("image_url"));
+                sliderItem.setDescription(detailsObject.getString("description"));
+                sliderItemList.add(sliderItem);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Error 3"+e.toString(), Toast.LENGTH_SHORT).show();
+            }
         }
-        ViewList(arrayList);
+        adapter.renewItems(sliderItemList);
     }
 
-    private void ViewList(ArrayList<String> array){
-        listAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_single_choice,array);
-        shoplist.setAdapter(listAdapter);
-        shoplist.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-    }*/
+    private void imageSlider(){
+        sliderView = findViewById(R.id.imageSlider);
+
+
+        adapter = new SliderAdapter(this);
+        sliderView.setSliderAdapter(adapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        sliderView.setIndicatorSelectedColor(Color.WHITE);
+        sliderView.setIndicatorUnselectedColor(Color.GRAY);
+        sliderView.setScrollTimeInSec(3);
+        sliderView.setAutoCycle(true);
+        sliderView.startAutoCycle();
+
+        extractBanners();
+
+        sliderView.setOnIndicatorClickListener(new DrawController.ClickListener() {
+            @Override
+            public void onIndicatorClicked(int position) {
+                Log.i("GGG", "onIndicatorClicked: " + sliderView.getCurrentPagePosition());
+            }
+        });
+    }
 
     private void setShop(JSONArray data) {
         for (int i = 0; i < data.length(); i++) {
